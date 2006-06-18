be15bfa796c680a3d7266493f112f7e6af073465
Jon Gettler <gettler@acm.org>
2006-06-18 18:45:20 -0500

    Add the 0.3.0 release.

--- htdocs/downloads.php
+++ htdocs/downloads.php
@@ -20,6 +20,7 @@
    <li><a href="https://sourceforge.net/project/showfiles.php?group_id=103474&package_id=111160&release_id=356389">mvpmc 0.1.8</a> (2005-09-13)</li>
    <li><a href="http://sourceforge.net/project/showfiles.php?group_id=103474&package_id=111160&release_id=368091">mvpmc 0.2.0</a> (2005-11-02)</li>
    <li><a href="http://sourceforge.net/project/showfiles.php?group_id=103474&package_id=111160&release_id=392824">mvpmc 0.2.1</a> (2006-02-11)</li>
+   <li><a href="http://sourceforge.net/project/showfiles.php?group_id=103474&package_id=111160&release_id=425148">mvpmc 0.3.0</a> (2006-06-15)</li>
    </ul>
 
 </div>
