53e7654543c3ce0605ebc85e9247bb0992085d8d
Jon Gettler <gettler@mvpmc.org>
2006-08-16 22:14:20 -0400

    Initial changes for moving the site to mvpmc.org.

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,44 @@
-<?PHP
-header("Location: idx.php?pg=main");
-?> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+	<meta name="author" content="Jon Gettler" />
+	<meta name="keywords" content="mvpmc, mediamvp, mediamvp media center, mythtv, replaytv" />
+	<meta name="description" content="The MediaMVP Media Center, a media player for the Hauppauge MediaMVP." />
+	<meta name="robots" content="all" />
+
+	<link rel="icon" href="http://mvpmc.org/favicon.ico" />
+	<link rel="shortcut icon" href="http://mvpmc.org/favicon.ico" />
+
+	<title>MediaMVP Media Center</title>
+	
+	<link rel="stylesheet" type="text/css" title="Default" href="mvpmc.css" />
+	<link rel="alternate stylesheet" type="text/css" title="Minimal" href="minimal.css" />
+
+</head>
+
+<body>
+
+  <div id="container">
+
+    <?PHP include("header.html"); ?>
+
+    <div id="supportingText">
+
+      <?PHP if(isset($_GET['pg']))
+                { include($_GET['pg'].".php"); }
+            else
+                { include("main.php"); }
+      ?>
+
+    </div>
+
+    <?PHP include("links.html"); ?>
+
+  </div>
+
+</body>
+
+</html>
+
