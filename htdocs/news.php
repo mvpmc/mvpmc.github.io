d525b4a47a7d470a38bcd5505ba21a0229fd9a69
Jon Gettler <gettler@mvpmc.org>
2011-12-18 16:36:53 -0600

    Switch the website from using PHP to Jekyll.

--- htdocs/news.php
+++
@@ -1,8 +0,0 @@
-<div class="title">
-
-  <h3><span>News</span></h3>
-
-</div>
-
-<?PHP include("news.html"); ?>
-
--- htdocs/settings.php
+++
@@ -1,8 +0,0 @@
-<div class="title">
-
-  <h3><span>Settings</span></h3>
-
-</div>
-
-<?PHP include("settings.html"); ?>
-
